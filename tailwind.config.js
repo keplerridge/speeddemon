cded4dc71b1bf9f6177360caf1872476b1b4685e
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-10-15 10:34:02 -0600

    added project structure with routes, pinia, tailwind, and vite test

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}", // Add Vue-related file extensions
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
