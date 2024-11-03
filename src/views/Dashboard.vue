89d8804fb0e821ac4951ccbde3681f80caaa5421
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-11-02 19:42:20 -0600

    added newActivityForm component

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,16 +2,11 @@
 import NewActivityForm from "../components/NewActivityForm.vue";
 
 export default {
-  components: {
-    NewActivityForm, // Register it here
-  },
+  components: {},
 };
 </script>
 
 <template>
   <h1 class="text-5xl text-slate-100">Dashboard</h1>
-  <main>
-    <NewActivityForm />
-    <!-- Now it should render correctly -->
-  </main>
+  <main></main>
 </template>
