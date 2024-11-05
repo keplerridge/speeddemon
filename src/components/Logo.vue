945828282d64eddbc8b0df38f85a4b5f633915c5
scjk25 <131284882+scjk25@users.noreply.github.com>
2024-11-05 08:10:47 -0700

    implementing logo

---
+++ src/components/Logo.vue
@@ -0,0 +1,22 @@
+<template>
+  <header>
+    <img :src="logo" alt="/Users/scottkishpaugh/Documents/Speed_demon_logo" />
+    <!-- Other header content -->
+  </header>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      logo: require('/Users/scottkishpaugh/Documents/Speed_demon_logo') // Path to the logo file
+    };
+  }
+};
+</script>
+
+<style scoped>
+/* Optional: Add styling for your logo */
+img {
+  height: 50px; /* Adjust size as needed */
+}
