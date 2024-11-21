246e4b78b1eb855d9a68c00e76289a89f308ed38
funnyman569 <168005402+funnyman569@users.noreply.github.com>
2024-11-21 10:00:15 -0700

    Additional Fix

--- database/functions-and-procedures.sql
+++ database/functions-and-procedures.sql
@@ -155,11 +155,4 @@ CALL InsertActivityLogData(
     'bike'
 );
 
-DO $$
-DECLARE
-    r RECORD;
-BEGIN
-    FOR r IN (SELECT tablename FROM pg_tables WHERE schemaname = 'speeddemonschema') LOOP
-        EXECUTE 'TRUNCATE TABLE speeddemonschema.' || r.tablename || ' RESTART IDENTITY CASCADE';
-    END LOOP;
-END $$;
+
