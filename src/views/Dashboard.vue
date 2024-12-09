f5ed6e152bad9148f989367759a73bb0b8b84f4c
Keaton Flake <91174706+keatonflake@users.noreply.github.com>
2024-12-09 14:36:35 -0700

    added timer changes

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -1,20 +1,19 @@
 //
 <script>
-import ActivityCard from "/Users/haydenh/Documents/GitHub/speeddemon/src/components/ActivityCard.vue";
+import ActivityCard from "../components/ActivityCard.vue";
 
 export default {
-  name: 'Dashboard',
-  components: { 
-    ActivityCard 
+  name: "Dashboard",
+  components: {
+    ActivityCard,
   },
 };
-
 </script>
 
 <template>
   <h1 class="text-5xl text-slate-100 text-center p-3">Public Dashboard</h1>
   <h3 class="text-3xl text-slate-100 text-center">View Recent Speed Demons</h3>
   <main class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 p-4">
-    <ActivityCard/>
+    <ActivityCard />
   </main>
 </template>
