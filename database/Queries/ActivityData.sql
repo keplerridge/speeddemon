f8d16c765b95e9ad5c4e13eeed3daf32181838cc
funnyman569 <168005402+funnyman569@users.noreply.github.com>
2024-12-09 22:16:09 -0700

    changed formatting of distanced_travaled to be only 3 decimal places

--- database/Queries/ActivityData.sql
+++ database/Queries/ActivityData.sql
@@ -4,7 +4,7 @@ SELECT
     activity_log.route_name AS "route_name",
     activity_log.mode_of_transport AS "mode_of_transport",  -- Ensure this column is in activity_log
     activity_log.time_elapsed AS "time_elapsed",  -- Renamed to avoid confusion with SQL TIME keyword
-    activity_log.distance_traveled AS "distance_traveled",
+    round(cast(activity_log.distance_traveled AS numeric), 3) AS "distance_traveled",
     location_tracker.start_point AS "start_point",
     location_tracker.end_point AS "end_point"
 FROM speeddemonschema.activity_log AS activity_log
