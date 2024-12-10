b93ee9119dd1db8481acc657d788276913ad2066
funnyman569 <168005402+funnyman569@users.noreply.github.com>
2024-12-09 22:40:29 -0700

    removed unncessary info instances in acitivtycard.vue

--- src/components/ActivityCard.vue
+++ src/components/ActivityCard.vue
@@ -23,15 +23,19 @@ export default {
                 const response = await axios.get(`http://localhost:3000/database/query?queryName=ActivityData`); // Replace "ActivityData with queryName you want"
                 //response.data = makes an array of objects from the response
                 //.map() = creates a new arrray with the shortened names as specified on the left hand side of the request
+
                 this.activityInfo = response.data.map (activity => ({
                     routename: activity.routename,
                     username: activity.username,
-                    email: activity.email,
-                    password: activity.password,
                     distance: activity.distance_traveled,
                     timeElapsed: activity.time_elapsed,
                     modeOfTransport: activity.mode_of_transport
+
                 })) ;  // Store the response data in activities
+
+                console.log(this.activityInfo); //debugging
+                
+
             } catch (error) {
                 console.error('Error fetching activity data:', error);
             }
@@ -48,9 +52,9 @@ export default {
                 v-for="activity in activityInfo"
                 :key="activity.id"
                 class="p-4 border rounded mb-2 bg-speedDemon-darkBlue border-speedDemon-red border-x-speedDemon-lightBlue border-4 border-double" >
-                <p class="italic underline text-g uppercase text-speedDemon-orange flex justify-center items-center">routename {{ activity.routename }}</p>
+                <p><strong class="italic underline text-g uppercase text-speedDemon-orange flex justify-center items-center">Route Name:</strong> {{ activity.routename }}</p>
                 <p><strong class="italic underline text-lg uppercase text-speedDemon-orange">Username:</strong> {{ activity.username }}</p>
-                <p><strong class="italic underline text-lg uppercase text-speedDemon-orange">Distance:</strong> {{ activity.distance }}</p>
+                <p><strong class="italic underline text-lg uppercase text-speedDemon-orange">Distance:</strong> {{ activity.distance }} miles </p>
                 <p><strong class="italic underline text-lg uppercase text-speedDemon-orange">Time:</strong> {{ activity.timeElapsed }}</p>
                 <p><strong class="italic underline text-lg uppercase text-speedDemon-orange">Mode of Transport:</strong> {{ activity.modeOfTransport }}</p>
             </li>
